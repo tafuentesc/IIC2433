abf73f7b196b1874c1ca6fde71edc0de1afe1190
:tomas :fuentes <:tafuentesc@gmail.com>
2013-09-11 03:20:09 -0400

    Se agrega for para ejecutar todas las posibles combinaciones en la parte 3 (Fisher Discriminant).

--- IIC2433_T1/src/part1.py
+++ IIC2433_T1/src/part1.py
@@ -122,39 +122,35 @@ def part1():
     ## ===================================================================
     
     # Primero, separamos la informacion en 2 clases
-    j = 0 # La idea es meterlo todo en un for
-    data2 = [[data[i] for i in range(datalen) if labels[i]==classes[j]],[data[i] for i in range(datalen) if labels[i]!=classes[j]]]
+    for j in range(len(classes)):
+        data2 = [[data[i] for i in range(datalen) if labels[i]==classes[j]],[data[i] for i in range(datalen) if labels[i]!=classes[j]]]
+        
+        # Extraemos las medias para los 2 sets de datos:
+        array2 = [numpy.array(data2[0]), numpy.array(data2[1])]
+        means2 = [array2[0].mean(0), array2[1].mean(0)]
     
-    # Extraemos las medias para los 2 sets de datos:
-    array2 = [numpy.array(data2[0]), numpy.array(data2[1])]
-    means2 = [array2[0].mean(0), array2[1].mean(0)]
-
-    print means2
+        print means2
+        
+        # Normalizamos los datos (restamos las medias):
+        B1 = data2[0] - means2[0]
+        B2 = data2[1] - means2[1]
     
-    # Normalizamos los datos (restamos las medias):
-    B1 = data2[0] - means2[0]
-    B2 = data2[1] - means2[1]
-
-    # Hecho esto, procedemos a calcular las sparse matrices:
-    S1 = dot(B1.transpose(), B1)
-    S2 = dot(B2.transpose(), B2)
-    
-    # Calculamos Sw, que mide varianza dentro de las clases:
-    Sw = S1 + S2
-    
-    # Finalmente, resolvemos para obtener la direccion de maxima separacion, V:
-    Sw_inv = linalg.inv(Sw)
-    
-    V = dot(Sw_inv, (means2[0] - means2[1]))
-    
-    # Obtenido V, procedemos a proyectar los datos y graficar:
-    
-    fisher1 = dot(V.transpose(), array2[0].transpose())    
-    fisher2 = dot(V.transpose(), array2[1].transpose())    
-    
-    plt.plot(fisher1, 'ro', fisher2, 'bo')
-    plt.show()
-    
-    print Sw_inv
-    print means2[0] - means2[1]
-    print V
+        # Hecho esto, procedemos a calcular las sparse matrices:
+        S1 = dot(B1.transpose(), B1)
+        S2 = dot(B2.transpose(), B2)
+        
+        # Calculamos Sw, que mide varianza dentro de las clases:
+        Sw = S1 + S2
+        
+        # Finalmente, resolvemos para obtener la direccion de maxima separacion, V:
+        Sw_inv = linalg.inv(Sw)
+        
+        V = dot(Sw_inv, (means2[0] - means2[1]))
+        
+        # Obtenido V, procedemos a proyectar los datos y graficar:
+        
+        fisher1 = dot(V.transpose(), array2[0].transpose())    
+        fisher2 = dot(V.transpose(), array2[1].transpose())    
+        
+        plt.plot(fisher1, 'ro', fisher2, 'bo')
+        plt.show()
